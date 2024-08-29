d160b3086a95aece8cb6e08209992bef0b5e6795
Daniel <daniel.aguilar7255@gmail.com>
2024-08-29 15:28:59 -0600

    Se agrega SharedPreferences, que maneja el guardado de sesion de MainActivity

--- app/src/main/java/mx/tec/login_shared/InicioActivity.kt
+++ app/src/main/java/mx/tec/login_shared/InicioActivity.kt
@@ -3,9 +3,9 @@ package mx.tec.login_shared
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class InicioActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicio)
+    class InicioActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_inicio)
+        }
     }
-}
--- app/src/main/java/mx/tec/login_shared/MainActivity.kt
+++ app/src/main/java/mx/tec/login_shared/MainActivity.kt
@@ -1,5 +1,6 @@
 package mx.tec.login_shared
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,13 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private val correctUser = "admin"
     private val correctPassword = "12345"
-    //Lateinit
     private lateinit var userText: EditText
     private lateinit var passwordText: EditText
     private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sharedPreferences = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        if (isLoggedIn) {
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_main)
         userText = findViewById(R.id.userText)
         passwordText = findViewById(R.id.passwordText)
@@ -25,9 +35,14 @@ class MainActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             val inputUser = userText.text.toString()
             val inputPassword = passwordText.text.toString()
+
             if (inputUser == correctUser && inputPassword == correctPassword) {
+                val editor = sharedPreferences.edit()
+                editor.putBoolean("isLoggedIn", true)
+                editor.apply()
                 val intent = Intent(this, InicioActivity::class.java)
                 startActivity(intent)
+                finish()
             } else {
                 Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
             }
