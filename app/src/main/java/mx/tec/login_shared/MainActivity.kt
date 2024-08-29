7ee131b3e0f671eb272e1f1917e33ca40fbb1900
Daniel Aguilar <daniel.aguilar7255@gmail.com>
2024-08-29 10:14:55 -0600

    Boton Inicar sesion cambia de pantalla, sin verificar credenciales

--- app/src/main/java/mx/tec/login_shared/MainActivity.kt
+++ app/src/main/java/mx/tec/login_shared/MainActivity.kt
@@ -1,20 +1,20 @@
 package mx.tec.login_shared
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loginButton = findViewById(R.id.button2)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
         }
     }
 }
