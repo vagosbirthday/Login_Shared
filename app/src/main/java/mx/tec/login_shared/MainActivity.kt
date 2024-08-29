333839c0f36153a20e537436e1c16f7b734dfe9d
Daniel <daniel.aguilar7255@gmail.com>
2024-08-29 15:05:30 -0600

    Se agrega Uusario y Contraseña, y sus verificaciones

--- app/src/main/java/mx/tec/login_shared/MainActivity.kt
+++ app/src/main/java/mx/tec/login_shared/MainActivity.kt
@@ -3,18 +3,36 @@ package mx.tec.login_shared
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val correctUser = "admin"
+    private val correctPassword = "12345"
+    //Lateinit
+    private lateinit var userText: EditText
+    private lateinit var passwordText: EditText
     private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userText = findViewById(R.id.userText)
+        passwordText = findViewById(R.id.passwordText)
         loginButton = findViewById(R.id.button2)
+
         loginButton.setOnClickListener {
-            val intent = Intent(this, InicioActivity::class.java)
-            startActivity(intent)
+            val inputUser = userText.text.toString()
+            val inputPassword = passwordText.text.toString()
+            if (inputUser == correctUser && inputPassword == correctPassword) {
+                val intent = Intent(this, InicioActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
+
+
